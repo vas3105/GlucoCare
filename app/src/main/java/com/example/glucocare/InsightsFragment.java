99259792d488c57604a10be86687c3a6397cdb88
vas3105 <vasavi.s3105@gmail.com>
2026-03-28 11:05:44 +0530

    Initial project setup with base template

--- app/src/main/java/com/example/glucocare/InsightsFragment.java
+++ app/src/main/java/com/example/glucocare/InsightsFragment.java
@@ -15,12 +15,12 @@ import android.view.ViewGroup;
  */
 public class InsightsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
+    //  Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+    // Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -36,7 +36,7 @@ public class InsightsFragment extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment InsightsFragment.
      */
-    // TODO: Rename and change types and number of parameters
+    //Rename and change types and number of parameters
     public static InsightsFragment newInstance(String param1, String param2) {
         InsightsFragment fragment = new InsightsFragment();
         Bundle args = new Bundle();
